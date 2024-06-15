f706234b492da81981f47e1218ffb15a1bfdb7d7
iqbal <iqbal.ahmed@xische.com>
2024-06-15 13:30:02 +0400

    Initial Commit

---
+++ app/src/main/java/com/dastanapps/visionai/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dastanapps.visionai
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.dastanapps.visionai.chat.ChatRoute
+import com.dastanapps.visionai.ui.theme.AiVisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AiVisionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    ChatRoute()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dastanapps/visionai/chat/ChatViewModel.kt
@@ -0,0 +1,71 @@
+package com.dastanapps.visionai.chat
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.asTextOrNull
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ChatViewModel(
+    generativeModel: GenerativeModel
+) : ViewModel() {
+    private val chat = generativeModel.startChat(
+        history = listOf(
+            content(role = Participant.MODEL.name) { text("Hi, I am Vision AI.\nHow can I help you?") }
+        )
+    )
+
+    private val _uiState: MutableStateFlow<ChatUiState> =
+        MutableStateFlow(ChatUiState(chat.history.map { content ->
+            // Map the initial messages
+            ChatMessage(
+                text = content.parts.first().asTextOrNull() ?: "",
+                participant = if (content.role == Participant.MODEL.name) Participant.USER else Participant.MODEL,
+                isPending = false
+            )
+        }))
+    val uiState: StateFlow<ChatUiState> =
+        _uiState.asStateFlow()
+
+
+    fun sendMessage(userMessage: String) {
+        // Add a pending message
+        _uiState.value.addMessage(
+            ChatMessage(
+                text = userMessage,
+                participant = Participant.USER,
+                isPending = true
+            )
+        )
+
+        viewModelScope.launch {
+            try {
+                val response = chat.sendMessage(userMessage)
+
+                _uiState.value.replaceLastPendingMessage()
+
+                response.text?.let { modelResponse ->
+                    _uiState.value.addMessage(
+                        ChatMessage(
+                            text = modelResponse,
+                            participant = Participant.MODEL,
+                            isPending = false
+                        )
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.value.replaceLastPendingMessage()
+                _uiState.value.addMessage(
+                    ChatMessage(
+                        text = e.localizedMessage,
+                        participant = Participant.ERROR
+                    )
+                )
+            }
+        }
+    }
+}
